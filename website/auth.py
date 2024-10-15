05951d7ff91ce962b7c42a6099373471f3a4b753
ChegeMicheal <chegemicheal003@gmail.com>
2024-10-15 16:17:13 +0300

    admin

--- website/auth.py
+++ website/auth.py
@@ -1571,6 +1571,10 @@ def search():
 def search404():
     return render_template('search404.html', user=current_user)
 
+@auth.route('/terms_and_conditions')
+def terms_and_conditions():
+    return render_template('terms_and_conditions.html', user=current_user)
+
 #create custom error page(s)
 
 #invalid url
